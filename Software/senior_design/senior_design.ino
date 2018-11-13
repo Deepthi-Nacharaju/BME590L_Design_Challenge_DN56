9fa53dc6f5f54cb6b88f57176e329348633fc82a
Deepthi Nacharaju <dnacharaju@gmail.com>
2018-11-13 10:45:18 -0500

    update PCB layout with LED

--- Software/senior_design/senior_design.ino
+++ Software/senior_design/senior_design.ino
@@ -1,3 +1,6 @@
+#include <avr/io.h>
+#include <avr/interrupt.h>
+
 #define LED 1
 #define BUTTON 2
 #define BLINK_RATE 2; //Hz
@@ -9,42 +12,23 @@ int old_button_state = 0;
 int operating_mode = 0;
 int debounce_delay = 250;
 int debounce_time = 0;
-
+bool pin_mode = LOW;
 bool _update = false;
 
 void setup() {
   // put your setup code here, to run once:
   pinMode(LED, OUTPUT);
-  pinMode(BUTTON, INPUT_PULLUP);
-  //attachInterrupt(BUTTON, blink, RISING);
+  digitalWrite(LED, LOW);
+  pinMode(BUTTON, INPUT);
   GIMSK = 0b00100000;
-  PCMSK = 0b0010011;
+  PCMSK = 0b0000100;
   sei();
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   debounce_check();
-  if (_update == true) {
-    switch (operating_mode) {
-      case 0:
-        SetBrightness(0);
-        break;
-      case 1:
-        SetBrightness(PWM_MAX / 3);
-        break;
-      case 2:
-        SetBrightness(PWM_MAX * 2 / 3);
-        break;
-      case 3:
-        SetBrightness(PWM_MAX);
-        break;
-      case 4:
-        Flash_Light();
-        break;
-    }
-    _update = false;
-  }
+  Update_LED();
 }
 
 ISR(PCINT0_vect) {
@@ -52,7 +36,7 @@ ISR(PCINT0_vect) {
 }
 
 void debounce_check() {
-  if (button_state != old_button_state) {
+  if (button_state - old_button_state > 1) {
     if (millis() - debounce_time > debounce_delay) {
       operating_mode += 1;
       operating_mode %= 5;
@@ -61,6 +45,7 @@ void debounce_check() {
       old_button_state = button_state;
     }
   }
+  //button_state%1000;
 }
 
 void SetBrightness(int intensity) {
@@ -70,11 +55,53 @@ void SetBrightness(int intensity) {
 void Flash_Light() {
   for (int i = 0; i <= PWM_MAX / 2; i++) {
     SetBrightness(i);
-    delay(2);
+    delay(1);
   }
   for (int i = PWM_MAX / 2 ; i >= 0; i--) {
     SetBrightness(i);
-    delay(2);
+    delay(1);
+  }
+}
+
+void Update_LED(){
+    if (_update == true) {
+    switch (operating_mode) {
+      case 0:
+        SetBrightness(0);
+        break;
+      case 1:
+        SetBrightness(1);
+        break;
+      case 2:
+        SetBrightness(10);
+        break;
+      case 3:
+        SetBrightness(PWM_MAX);
+        break;
+      case 4:
+        while(operating_mode == 4){
+        Flash_Light();
+        debounce_check();
+        }
+        break;
+    }
+    //delay(500);
+    _update = false;
+  }
+}
+
+void two_state_LED(){
+   if ((_update == true) && (pin_mode == HIGH)){
+    pin_mode = LOW;
+    digitalWrite(LED, pin_mode);
+    _update == false;
+    delay(500);
+  }
+  else if ((_update == true) && (pin_mode == LOW)){
+    pin_mode = HIGH;
+    digitalWrite(LED, pin_mode);
+    _update == false;
+    delay(500);
   }
 }
 
